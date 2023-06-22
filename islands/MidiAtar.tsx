9d8ba9fa7f43e7e2062d5bf7a5083b1fc0b6aa7d
Unusual Norm <40722525+UnusualNorm@users.noreply.github.com>
2023-06-22 22:50:02 +0000

    Midi Player, without the typescript

--- islands/MidiAtar.tsx
+++ islands/MidiAtar.tsx
@@ -1,8 +1,11 @@
 import type { JSX } from "preact";
 import { IS_BROWSER } from "$fresh/runtime.ts";
-import { useEffect, useState } from "preact/hooks";
+import { useEffect, useRef, useState } from "preact/hooks";
 import { useSocket } from "../utils/socket.io.ts";
+import MidiPlayer from "midi-player-js";
 
+import Input from "./Input.tsx";
+import Button from "./Button.tsx";
 import ControlledPiano from "./Piano/ControlledPiano.tsx";
 import { NoteRange } from "../types/piano.ts";
 
@@ -63,6 +66,9 @@ const MidiAtar = ({
   // We don't need to grab an idr frame, as music notes don't last for very long
   const [activeNotes, setActiveNotes] = useState<number[]>([]);
   const [useractiveNotes, setUserActiveNotes] = useState<number[]>([]);
+  const [uploadingMidi, setUploadingMidi] = useState(false);
+  const [playingMidi, setPlayingMidi] = useState(false);
+  const playerRef = useRef<any>();
 
   useEffect(() => {
     if (!socket) {
@@ -106,21 +112,106 @@ const MidiAtar = ({
       return useractiveNotes.filter((note) => midiNumber !== note);
     });
 
+  useEffect(() => {
+    // Setup midi input
+    if (!navigator.requestMIDIAccess) {
+      return;
+    }
+
+    navigator.requestMIDIAccess().then((midiAccess) => {
+      const inputs = midiAccess.inputs.values();
+
+      for (const input of inputs) {
+        // @ts-ignore onmidimessage is not correct in the typescript definition
+        input.onmidimessage = ({ data }: MIDIMessageEvent) => {
+          const [command, note] = data;
+
+          if (command === 144) {
+            handlePlayNoteInput(note);
+          } else if (command === 128) {
+            handleStopNoteInput(note);
+          }
+        };
+      }
+    });
+  }, []);
+
   return (
-    <ControlledPiano
-      activeNotes={activeNotes.concat(useractiveNotes)}
-      onPlayNoteInput={handlePlayNoteInput}
-      onStopNoteInput={handleStopNoteInput}
-      noteRange={noteRange}
-      playNote={() => {}}
-      stopNote={() => {}}
-      renderNoteLabel={renderNoteLabel}
-      className={className}
-      disabled={disabled}
-      width={width}
-      keyWidthToHeight={keyWidthToHeight}
-      keyboardShortcuts={keyboardShortcuts}
-    />
+    <div class="flex flex-col items-center justify-center w-full max-w-md p-4">
+      <div class="flex flex-row items-center justify-center w-full">
+        <Input
+          type="file"
+          accept=".mid"
+          disabled={uploadingMidi}
+          onChange={(e) => {
+            playerRef.current?.stop();
+            if (!e.target) {
+              return;
+            }
+
+            setUploadingMidi(true);
+            const reader = new FileReader();
+            reader.onload = (e) => {
+              playerRef.current = new MidiPlayer.Player(
+                (
+                  event: { name: string; velocity: number; noteNumber: number },
+                ) => {
+                  // Note that because of a common practice called "running status" many MIDI files may use Note on events with 0 velocity in place of Note off events.
+                  if (event.name === "Note on" && event.velocity === 0) {
+                    event.name = "Note off";
+                  }
+
+                  if (event.name === "Note on") {
+                    handlePlayNoteInput(event.noteNumber);
+                  } else if (event.name === "Note off") {
+                    handleStopNoteInput(event.noteNumber);
+                  }
+                },
+              );
+              playerRef.current.loadArrayBuffer(reader.result as ArrayBuffer);
+              setUploadingMidi(false);
+            };
+
+            // @ts-ignore target is not correct in the typescript definition
+            reader.readAsArrayBuffer((e.target).files[0]);
+          }}
+        />
+        <Button
+          onClick={() => {
+            setPlayingMidi(!playingMidi);
+            if (playingMidi) {
+              playerRef.current?.stop();
+              useractiveNotes.forEach((note) => {
+                socket?.emit("midiAtarKey", note, false);
+              });
+              setUserActiveNotes([]);
+            } else {
+              playerRef.current?.play();
+            }
+          }}
+          disabled={uploadingMidi}
+          className="m-2"
+        >
+          {playingMidi ? "Stop" : "Play"}
+        </Button>
+      </div>
+      <div class="flex flex-row items-center justify-center w-full">
+        <ControlledPiano
+          activeNotes={activeNotes.concat(useractiveNotes)}
+          onPlayNoteInput={handlePlayNoteInput}
+          onStopNoteInput={handleStopNoteInput}
+          noteRange={noteRange}
+          playNote={() => {}}
+          stopNote={() => {}}
+          renderNoteLabel={renderNoteLabel}
+          className={className}
+          disabled={disabled}
+          width={width}
+          keyWidthToHeight={keyWidthToHeight}
+          keyboardShortcuts={keyboardShortcuts}
+        />
+      </div>
+    </div>
   );
 };
 
